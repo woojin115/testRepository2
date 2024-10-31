020ff1c680faaacc03a0279f26d58877179aa29c
woojin115 <woojinkim007@naver.com>
2024-10-31 09:46:19 +0900

    getter

--- TestProject/src/main/java/com/kh/git/notice/Notice.java
+++ TestProject/src/main/java/com/kh/git/notice/Notice.java
@@ -2,4 +2,35 @@ package com.kh.git.notice;
 
 public class Notice {
 	private String no;
+	private String gender;
+	private int age;
+	public Notice() {
+		super();
+	}
+	public Notice(String no, String gender, int age) {
+		super();
+		this.no = no;
+		this.gender = gender;
+		this.age = age;
+	}
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 }

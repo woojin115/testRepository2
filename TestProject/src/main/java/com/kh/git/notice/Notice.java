98485879d67c192f785aa5a001f37d0941269927
eossy1019 <eossy1019@gmail.com>
2024-10-31 09:50:55 +0900

    공지사항 vo 드디어 완성

--- TestProject/src/main/java/com/kh/git/notice/Notice.java
+++ TestProject/src/main/java/com/kh/git/notice/Notice.java
@@ -1,5 +1,37 @@
 package com.kh.git.notice;
 
+import java.util.Objects;
+
 public class Notice {
 	private String no;
+	private String title;
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(no, title);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Notice other = (Notice) obj;
+		return Objects.equals(no, other.no) && Objects.equals(title, other.title);
+	}
+	
+	
 }
